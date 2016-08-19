a61171f2b8b9cda484194e5df12bcadf549fcbd5
minggui.han <minggui.han@zhaopin.com.cn>
2016-08-19 15:27:01 +0800

    固定版本selenium2

---
+++ src/main/java/webtest/core/listenner/ReportListener.java
@@ -0,0 +1,18 @@
+package webtest.core.listenner;
+
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.xml.XmlSuite;
+
+import java.util.List;
+
+/**
+ * Created by hanminggui on 3/29/2016.
+ */
+public class ReportListener implements IReporter {
+
+    @Override
+    public void generateReport(List<XmlSuite> list, List<ISuite> list1, String s) {
+
+    }
+}
